154eb25296fb39546950f8396d6f44954841e3ef
Alexandr Lesnichenko <admin@webgoodwin.com>
2023-11-22 21:38:22 +0300

    clean main, wait output

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,52 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/superles/yapgofermart/internal/config"
-	"github.com/superles/yapgofermart/internal/server"
-	"github.com/superles/yapgofermart/internal/storage"
-	"github.com/superles/yapgofermart/internal/storage/pgstorage"
-	"github.com/superles/yapgofermart/internal/utils/logger"
+	"io"
 	"log"
+	"net/http"
 )
 
 func main() {
 	log.Println("start app")
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered in main", r)
-		}
-	}()
-	cfg, err := config.New()
-	if err != nil {
-		log.Fatal("ошибка инициализации конфига: ", err.Error())
-	}
-	if err = logger.Initialize(cfg.LogLevel); err != nil {
-		log.Fatal("ошибка инициализации logger: ", err.Error())
-	}
-
-	if len(cfg.Endpoint) == 0 {
-		log.Fatal("не настроен адрес запуска сервера")
-	}
-
-	if len(cfg.AccrualSystemAddress) == 0 {
-		log.Fatal("не настроен адрес системы расчёта")
-	}
-
-	if len(cfg.DatabaseDsn) == 0 {
-		log.Fatal("не настроена бд")
-	}
-
-	var store storage.Storage
-	if store, err = pgstorage.NewStorage(cfg.DatabaseDsn); err != nil {
-		log.Fatal("ошибка инициализации бд", err.Error())
-	}
-	logger.Log.Error("конфиг:", cfg)
-	srv := server.New(cfg, store)
-	appContext := context.Background()
-	err = srv.Run(appContext)
-	if err != nil {
-		log.Fatal("ошибка инициализации сервера: ", err.Error())
-	}
+	helloHandler := func(w http.ResponseWriter, req *http.Request) { io.WriteString(w, "Hello, world!\n") }
+	http.ListenAndServe("localhost:8080", helloHandler)
 }
